b1c7f961848e1e9244167f6f7634ff321a9e1c27
gesneriana <s694060865@163.com>
2017-03-24 23:57:00 +0800

    简批八字的视图页

--- WeiQingBaZiFengShuiSuanMing/Controllers/AdminController.cs
+++ WeiQingBaZiFengShuiSuanMing/Controllers/AdminController.cs
@@ -23,5 +23,27 @@ namespace WeiQingBaZiFengShuiSuanMing.Controllers
             }
             return View();
         }
+
+        /// <summary>
+        /// 简批八字的视图页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult JianPi(int id = 0)
+        {
+            if (id == 0)
+            {
+                return Redirect("/admin/index");
+            }
+            using (WeiQingEntities db = new WeiQingEntities())
+            {
+                var m = db.bazijianpi.Where(x => x.id == id).FirstOrDefault();
+                if (m != null && m.id > 0)
+                {
+                    ViewData["m"] = m;
+                    return View();
+                }
+            }
+            return Redirect("/admin/index");
+        }
     }
 }
