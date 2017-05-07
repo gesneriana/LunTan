73641e278df66a3dcd6886da8248544e6a6b2177
gesneriana <s694060865@163.com>
2017-05-07 15:47:36 +0800

    预测历史移动版

--- WeiQingBaZiFengShuiSuanMing/Controllers/m/Index_mController.cs
+++ WeiQingBaZiFengShuiSuanMing/Controllers/m/Index_mController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using EFDao.Entity;
+using Common.Utility;
 
 namespace WeiQingBaZiFengShuiSuanMing.Controllers.m
 {
@@ -26,6 +27,31 @@ namespace WeiQingBaZiFengShuiSuanMing.Controllers.m
                 {
                     var ycList = db.bazijianpi.Where(x => x.state == 1).OrderByDescending(x => x.addtime).Take(10).ToList();
                     ViewData["ycList"] = ycList;
+                }
+            }
+            return View();
+        }
+
+        public ActionResult yclist(int page=1,string key = "")
+        {
+            using (bazifengshuisuanmingEntities db = new bazifengshuisuanmingEntities())
+            {
+                if (!string.IsNullOrEmpty(key))
+                {
+                    // var ycList = db.bazijianpi.Where(x => x.state == 1 && (x.name.Contains(key) || x.born_place.Contains(key) || x.bazi.Contains(key))).OrderByDescending(x => x.addtime).Take(10).ToList();
+                    var q = db.bazijianpi.Where(x => x.state == 1 && (x.name.Contains(key) || x.born_place.Contains(key) || x.bazi.Contains(key))).OrderByDescending(x => x.addtime);
+                    var p = new EFPaging<bazijianpi>();
+                    var yclist = p.getPageList(q, "/index_m/yclist", page, 12);
+                    ViewData["url"] = p.pageUrl;
+                    ViewData["ycList"] = yclist;
+                }
+                else
+                {
+                    var q = db.bazijianpi.Where(x => x.state == 1).OrderByDescending(x => x.addtime);
+                    var p = new EFPaging<bazijianpi>();
+                    var yclist = p.getPageList(q, "/index_m/yclist", page, 12);
+                    ViewData["url"] = p.pageUrl;
+                    ViewData["ycList"] = yclist;
                 }
             }
             return View();
